1636e6c370b1bcd86b6aa159add7075669747d05
oveecookedcircuit <manaspatel31122004@gmail.com>
2024-04-20 21:51:49 -0400

    project init

---
+++ ecommerce_shop/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Start"),
+      ),
+      body: Center(
+        child: Text("This is the beginning of our team project"),
+      ),
+    );
+  }
+}
+
