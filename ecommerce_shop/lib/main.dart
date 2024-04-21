cf1876fd6d8c9e1602c2d4026876a62364bcd405
oveecookedcircuit <manaspatel31122004@gmail.com>
2024-04-21 10:41:43 -0400

    Account creation page

--- ecommerce_shop/lib/main.dart
+++ ecommerce_shop/lib/main.dart
@@ -11,11 +11,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: CreateAccountPage(),
     );
   }
 }
 
+
+//This is the login page when a user has an account
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -34,7 +36,7 @@ class _LoginPageState extends State<LoginPage> {
     // Check for valid credentials (for demonstration purpose, hardcoded)
     if (_email == '123@gmail.com' && _password == '123') {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Registration successful')),
+        SnackBar(content: Text('Login successful')),
       );
       Navigator.push(
         context,
@@ -80,6 +82,77 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
+//This is the account creation page
+class CreateAccountPage extends StatefulWidget {
+  const CreateAccountPage({super.key});
+
+  @override
+  State<CreateAccountPage> createState() => _CreateAccountPageState();
+}
+
+class _CreateAccountPageState extends State<CreateAccountPage> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _passConfirmController = TextEditingController();
+
+  void _create(){
+    String _email = _emailController.text.trim();
+    String _password = _passwordController.text.trim();
+    String _confirmPass = _passConfirmController.text.trim();
+
+    if (_password == _confirmPass) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Account created successfully')),
+      );
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Account creation failed')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Create Account"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: "Email"),
+            ),
+            SizedBox(height: 25.0,),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: "Password"),
+              obscureText: true,
+            ),
+            SizedBox(height: 25.0,),
+            TextField(
+              controller: _passConfirmController,
+              decoration: InputDecoration(labelText: "Confirm Password"),
+              obscureText: true,
+            ),
+            SizedBox(height: 25.0,),
+            ElevatedButton(onPressed: _create, child: Text("Create Account")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+//This is the 1st page after being logged in
 class HomePage extends StatelessWidget {
   final String email;
 
