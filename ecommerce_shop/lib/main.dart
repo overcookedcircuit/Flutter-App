fce188670035a36f5e385b1d3e6329ceb646b7b9
oveecookedcircuit <manaspatel31122004@gmail.com>
2024-04-30 15:01:48 -0400

    I resolved the issue FallThroughError() :D

--- ecommerce_shop/lib/main.dart
+++ ecommerce_shop/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ecommerce_shop/login/onBoardScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore_platform_interface/cloud_firestore_platform_interface.dart';
 
 void main(){
   runApp(const MyApp());
